b62ecdd85e8ad18247765aa8a29d60e31d77cf1c
Terence <terresquall@hotmail.com>
2022-06-14 15:56:09 +0800

    With the interface up.

--- main.py
+++ main.py
@@ -19,96 +19,198 @@ p = Punctuator('path/to/the/model'). You can see my example in the code.
 NOW, you are ready to use the script.
 """
 
-from punctuator import Punctuator
+# Import essential libraries.
 import os
+import json
 
-# Enforce Python version before this is run.
-import sys
-if sys.version_info.major != 3 or sys.version_info.minor != 9:
-    print("You need Python 3.9 for this script to work, as we need numpy=1.20.3, which only runs on Python 3.9.")
-    print("Exiting...")
-    exit()
+# # Try importing Punctuator.
+# try:
+	# from punctuator import Punctuator
+# except ModuleNotFoundError:
+	# print("Punctuator module not found! Run pip install punctuator to install it.")
 
-# Check if the correct version of numpy is installed.
-try:
-    import numpy
-except ModuleNotFoundError:
-    print("numpy is not installed. Run pip install numpy=1.20.3 to install it.")
-    print("Exiting...")
-    exit()
-    
-# Read the settings file to get the paths that we want.
-try:
-    import json
-    
-    with open("config.json") as config:
-        data = json.loads(config)
-    
-except ModuleNotFoundError:
+# # Enforce Python version before this is run.
+# import sys
+# if sys.version_info.major != 3 or sys.version_info.minor != 9:
+	# print("You need Python 3.9 for this script to work, as we need numpy=1.20.3, which only runs on Python 3.9.")
+	# print("Exiting...")
+	# exit()
 
-    print("json module not found.")
-    
-except FileNotFoundError:
-    
-    # If the config file doesn't exist, create it.
-    data = {
-        "models": {
-            "directory": "models"
-        },
-        "inputDirectory": "inputs",
-        "outputDirectory": "outputs"
-    }
-    
-    # Write a config file if there isn't one.
-    with open("config.json","w") as config:
-        config.write(json.dumps(data, indent=4))
-        print("Created a config.json file for adjusting which models are to be used.")
-    
-# If the data array has nothing, ask the user which model he wants to use.
+# # Check if the correct version of numpy is installed.
+# try:
+	# import numpy
+# except ModuleNotFoundError:
+	# print("numpy is not installed. Run pip install numpy=1.20.3 to install it.")
+	# print("Exiting...")
+	# exit()
+ 
+   
+class Punktuator:
+	
+	def __init__(self):
+		self.config = self.initConfig()
+		self.modelPath = self.determineModel()
+		self.inputPath = self.determineInputFile()
+		self.outputPath = self.determineOutputPath()
+		
+	def initConfig(self):
+		# Read the settings file to get the paths that we want.
+		try:		
+			with open("config.json") as config:
+				return json.loads(config.read())
+			
+		except ModuleNotFoundError:
 
-
-model_found = False  # variable to check whether model is correctly set or not.
-try:
-    p = Punctuator('Demo-Europarl-EN.pcl')  # Defining the model that we want to use.
-    model_found = True
-except AssertionError:
-    print("-" * 80)
-    print("Sorry, but the directory of the model that you are trying to set doesn't exist."
-          "\nCheck the directory again, please.")
-    print("-" * 80)
-
-
-if model_found:
-    try:
-        with open('shader-subscriber.txt', 'r+') as f:
-            subtitles = f.readlines()
-            f.seek(0)
-            text = ''
-            timings = []
-            lengths = []
-            for i in range(len(subtitles)):
-                if i % 3 == 0:  # Adding timings for the separate Array
-                    subtitles[i] = subtitles[i].strip()
-                    timings.append(subtitles[i])
-                    continue
-                if subtitles[i] == '\n':  # Skipping the new lines
-                    continue
-                subtitles[i] = subtitles[i].replace('\n',
-                                                    ' ')  # in the end of each line, there is \n, so I am changing it to space
-                text += subtitles[i]
-            f.close()
-            print(text)  # Just checking the text
-            corrected_text = p.punctuate(text)  # punctuate method is the one who makes punctuation on the text.
-            print(corrected_text)  # Just checking the corrected text
-            with open('C:/Users/umral/Desktop/captions.sbv', 'w') as f:  # Writing to the file
-                f.write(corrected_text)
-                f.close()
-    except FileNotFoundError:
-        print("-" * 43)
-        print('Error with directory of the captions file.'
-              '\nCheck its location again please.')
-        print("-" * 43)
-    except:
-        print("-" * 100)
-        print("Another error occurred, but the current error is not related to the location of the captions file.")
-        print("-" * 100)
+			print("json module not found.")
+			
+		except FileNotFoundError:
+			return self.regenerateConfig()
+			
+		except json.decoder.JSONDecodeError:
+			return self.regenerateConfig()
+			
+	def regenerateConfig(self):
+	
+		# If the config file doesn't exist or is malformed, recreate it.
+		data = {
+			"models": {
+				"directory": "models"
+			},
+			"inputDirectory": "inputs",
+			"outputDirectory": "outputs"
+		}
+		
+		# Write a config file if there isn't one.
+		with open("config.json","w") as config:
+			config.write(json.dumps(data, indent=4))
+			print("Created a config.json file for adjusting which models are to be used.")
+			
+		return data
+		
+	# Ask the user which model he wants to use.
+	def determineModel(self):
+		try:
+			files = os.listdir(self.config["models"]["directory"])
+			
+			# Select a model to use.
+			print("\nSelect a model to use:\n")
+			
+			index = 1
+			models = []
+			for f in files:
+				fdat = os.path.splitext(f)
+				if fdat[1].lower() == '.pcl':
+					print("{:d}. {:s}".format(index,f))
+					models.append(f)
+					index += 1
+			
+			# Get the model to use.
+			if len(models) > 0:
+			
+				modelSelected = int(input("\nEnter model number: "))
+				
+				if modelSelected > len(models):
+					print("Invalid model selected!")
+					return self.determineModel()
+				
+				selectedModel = models[modelSelected - 1]
+				print("You've selected {:s}.\n".format(selectedModel))
+				return self.config["models"]["directory"] + os.path.sep + selectedModel
+				
+			else:
+				print("There are no models for Punctuator to use. Please download them and put them in {:s}.".format(os.getcwd() + os.path.sep + self.config["models"]["directory"]))
+				
+		except FileNotFoundError:
+			os.mkdir(self.config["models"]["directory"])
+			print("Models directory not found. Created a directory.")
+			
+			# Run the function again
+			return self.determineModel()
+				
+	# Ask the user which model he wants to use.
+	def determineInputFile(self):
+		try:
+			files = os.listdir(self.config["inputDirectory"])
+			
+			# Select a model to use.
+			print("\nWhich file do you want to punctuate?\n")
+			
+			index = 1
+			inputs = []
+			for f in files:
+				print("{:d}. {:s}".format(index,f))
+				inputs.append(f)
+				index += 1
+				
+			# Get the model to use.
+			if len(inputs) > 0:
+			
+				fileSelected = int(input("\nSelect a file: "))
+				
+				if fileSelected > len(inputs):
+					print("Invalid file selected!")
+					return self.determineInputFile()
+				
+				selectedInput = inputs[fileSelected - 1]
+				print("You've selected {:s}.\n".format(selectedInput))
+				return self.config["inputDirectory"] + os.path.sep + selectedInput
+				
+			else:
+				print("There are no input files!")
+				
+		except FileNotFoundError:
+			os.mkdir(self.config["inputDirectory"])
+			print("Input directory not found. Created the directory.")
+			
+			# Run the function again
+			return self.determineInputFile()
+			
+	# Get the path to output to from the input path.
+	def determineOutputPath(self):
+		path = self.inputPath.replace(self.config["inputDirectory"],self.config["outputDirectory"])
+		print("File will be output to {:s}.".format(path))
+		return path
+			
+	def run(self):
+		
+		try:
+			with open(self.inputPath, 'r+') as f:
+			
+				subtitles = f.readlines()
+				f.seek(0)
+				text = ''
+				timings = []
+				lengths = []
+				
+				for i in range(len(subtitles)):
+					if i % 3 == 0:  # Adding timings for the separate Array
+						subtitles[i] = subtitles[i].strip()
+						timings.append(subtitles[i])
+						continue
+					if subtitles[i] == '\n': # Skipping the new lines
+						continue
+						
+					# in the end of each line, there is \n, so I am changing it to space
+					subtitles[i] = subtitles[i].replace('\n', ' ')
+					text += subtitles[i]
+				f.close()
+				
+				print(text)  # Just checking the text
+				corrected_text = p.punctuate(text)  # punctuate method is the one who makes punctuation on the text.
+				print(corrected_text)  # Just checking the corrected text
+				with open(self.outputPath, 'w') as f:  # Writing to the file
+					f.write(corrected_text)
+					print("File successfully output to {:s}.".format(self.outputPath))
+					
+		except FileNotFoundError:
+			print("-" * 43)
+			print('Error with directory of the captions file.'
+				  '\nCheck its location again please.')
+			print("-" * 43)
+		except:
+			print("-" * 100)
+			print("Another error occurred, but the current error is not related to the location of the captions file.")
+			print("-" * 100)
+		
+    
+Punktuator()   
